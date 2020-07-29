f61215322e95a14b8aa746bf59be8804c87d259f
Jenny Citherlet <jennycith@gmail.com>
2020-07-29 22:30:57 +0000

    Added moving keys

--- input.js
+++ input.js
@@ -1,14 +1,32 @@
+// Stores the active TCP connection object.
+let connection;
+
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
 
 const handleUserInput = function(key) {
-
-  if (key === '\u0003') { // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
     process.exit();
   }
-}
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+
+  }
+
+};
 
 
 
