251352cc30f6781a0f4b23c03ea4231f51a04cbd
Jenny Citherlet <jennycith@gmail.com>
2020-07-29 20:04:44 +0000

    Inital commit of module with connect function

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+
+/*
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50541
+  });
+
+
+  conn.setEncoding('utf8'); // interpret incoming data as text
+
+  conn.on('data', message => {
+    console.log(message);
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: JC");
+  });
+
+  return conn;
+}
+module.exports = { connect };
