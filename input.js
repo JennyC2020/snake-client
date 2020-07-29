cec8e88b6bd95920d78f00c1749693776cba7fbd
Jenny Citherlet <jennycith@gmail.com>
2020-07-29 22:36:49 +0000

    Added personalized key with canned messages

--- input.js
+++ input.js
@@ -11,19 +11,28 @@ const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
+
   if (key === "w") {
     connection.write("Move: up");
   }
+
   if (key === "s") {
     connection.write("Move: down");
   }
+
   if (key === "a") {
     connection.write("Move: left");
-
   }
+
   if (key === "d") {
     connection.write("Move: right");
+  }
+  if (key === "h") {
+    connection.write("Say: Hi everyone");
+  }
 
+  if (key === "o") {
+    connection.write("Say: Have fun");
   }
 
 };
